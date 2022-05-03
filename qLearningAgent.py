cfbe97285510c183c18c472b26ec1612a311eca3
Scollison6 <43913888+Scollison6@users.noreply.github.com>
2022-05-03 10:25:45 -0500

    Update qLearningAgent.py
    changed updateQValues method and computeValueFromQvalues method

--- qLearningAgent.py
+++ qLearningAgent.py
@@ -12,10 +12,10 @@ class qLearningAgent(Player):
         self.qvalues = {} #list of q values indexed by (state, action), action being either 's' or 't'
         self.qvalues[(13, 's')] = 1000
         self.qvalues[(13, 't')] = -1000
-        self.qvalues[(1, 's')] = -1000
-        self.qvalues[(1, 't')] = 1000
+        self.qvalues[(0, 's')] = -1000
+        self.qvalues[(0, 't')] = 1000
         self.alpha = 0.2
-        self.discount = 0.3
+        self.discount = 0.1
 
     def turn(self, value, numPlayers, prevValue, actions):
         return self.computeActionFromQValues(value)
@@ -29,12 +29,16 @@ class qLearningAgent(Player):
             #print("I could not find the value %d %s" % (state, action))
             return 0.0
 
-    def updateQValues(self, state, action, nextState, reward):
+    def updateQValues(self, state, action, nextState, nextAction, reward):
+        print('\nUpdate:', state, action, nextState, nextAction, reward, '\n')
         curVal = self.getQValue(state, action)
-        nextVal = self.computeValueFromQValues(nextState)
-        
-        self.qvalues[(state, action)] = self.alpha * (reward + self.discount * nextVal) \
+        nextVal = self.computeValueFromQValues(nextState, nextAction)
+
+        if not((state == 0 and action == 't') or (state == 13 and action == 's') or (state == 0 and action == 's') or (state == 13 and action == 't')):
+            self.qvalues[(state, action)] = self.alpha * (reward + self.discount * nextVal) \
                                         + (1-self.alpha) * curVal
+
+        print('\nQVals\n', self.qvalues, '\n')
         
     def computeActionFromQValues(self, state):
         tradeVal = self.getQValue(state, 't')
@@ -47,11 +51,18 @@ class qLearningAgent(Player):
         else:
             return 't'
         
-    def computeValueFromQValues(self, state):
-        tradeVal = self.getQValue(state, 't')
-        stayVal = self.getQValue(state, 's')
-        
-        if (stayVal > tradeVal):
-            return stayVal
+    def computeValueFromQValues(self, state, action):
+        if state == 13:
+            return 1000
         else:
-            return tradeVal
+            return -1000
+
+        #return self.getQValue(state, action)
+
+        # tradeVal = self.getQValue(state, action)
+        # stayVal = self.getQValue(state, action)
+        #
+        # if (stayVal > tradeVal):
+        #     return stayVal
+        # else:
+        #     return tradeVal
